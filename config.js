d9f824729a4974feee7a016de1f3288429528d56
George Machibya <gman2them@gmail.com>
2024-04-20 23:53:45 +0300

    add the database connection

--- config.js
+++ config.js
@@ -7,10 +7,10 @@ import pg from "pg";
 const{ Pool } = pg;
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
+  user: process.env.DB_USER_ONLINE,
+  host: process.env.DB_HOST_ONLINE,
+  database: process.env.DB_DATABASE_ONLINE,
+  password: process.env.DB_PASSWORD_ONLINE,
   port: 5432,
 });
 
