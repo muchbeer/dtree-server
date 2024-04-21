874dcf87094164b061f88fb2f9b3e630eeaff8c9
George Machibya <gman2them@gmail.com>
2024-04-21 21:19:23 +0300

    add time using moment

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ import express from "express";
 
 import * as dotenv from 'dotenv';
 import { enableAirtime, getUsers, login, register } from "../controllers/user.js";
+import moment from "moment/moment.js";
 
 
 dotenv.config();
@@ -13,7 +14,8 @@ router.get('/time', (req, res) => {
     const options = { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone  };
       // const { timeZone } = new Date().toLocaleString('en', options);
       const timeWithZone = new Date().toLocaleString('en', 180 );
-    const timez = { toIsoString: serverTime, timeZone: timeWithZone }
+      const momenti = moment().format();
+    const timez = { toIsoString: serverTime, timeZone: timeWithZone, momentTime : momenti}
     return res.json({ isLogged: true, time: timez});  
 })
 
