51bae6d7d34f1c888b26d0994a98e9f72aa9ebb7
George Machibya <gman2them@gmail.com>
2024-04-21 22:20:36 +0300

    make it work hard way

--- routes/auth.js
+++ routes/auth.js
@@ -11,12 +11,25 @@ router.get('/time', (req, res) => {
     const serverTime = new Date().toISOString(); 
 
     const options = { timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone  };
-      // const { timeZone } = new Date().toLocaleString('en', options);
       const timeWithZone = new Date().toLocaleString();
-      const timeCorrection = new Date().toString();
-  
-      var createdDateTime = Date.now()
-    const timez = { toIsoString: serverTime, timeZone: timeWithZone, newTimeZ: createdDateTime };
+
+      // Current UTC time
+const utcDate = new Date();
+
+// Offset for GMT+3 in minutes (3 hours * 60 minutes)
+const offsetMinutes = 3 * 60;
+
+// Convert UTC to GMT+3
+const gmtPlus3Date = new Date(utcDate.getTime() + offsetMinutes * 60000);
+
+// Format the GMT+3 date
+const formattedDate = gmtPlus3Date.toISOString(); // You can format it as needed
+
+
+const utcformat = utcDate.toISOString()
+
+
+    const timez = { toIsoString: serverTime, utcTime : utcformat, gmtTime: formattedDate };
     return res.json({ isLogged: true, time: timez});  
 })
 
