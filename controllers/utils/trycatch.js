f257e843040cca7bf236d8fdd6ecc13a0f520825
George Machibya <gman2them@gmail.com>
2024-06-09 20:57:18 +0300

    check error on Bulk SMS

--- controllers/utils/trycatch.js
+++ controllers/utils/trycatch.js
@@ -3,7 +3,8 @@ const tryCatch = (controller) => {
       try {
         await controller(req, res);
       } catch (error) {
-        console.log(error);
+        console.log('The error cause is : ' + error);
+        console.log('This error from json is : ' + JSON.stringify(error))
         res.status(500).json({ success: false, message: 'Please check your network and try again',
         });
       }
