1b1c596fc8991362510dd61447672c4f7e660aa6
George Machibya <gman2them@gmail.com>
2024-07-02 02:10:00 +0300

    adding more header

--- index.js
+++ index.js
@@ -17,6 +17,17 @@ const app = express();
 
 app.use(express.json());
 
+const allowedOrigins = [process.env.CLIENT_SERVER_URL_HIGHLINK , process.env.CLIENT_SERVER_URL, 'http://localhost:3000',];
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,OPTIONS,POST,PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+/*
 app.use((req, res, next) => {
   //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.setHeader('Access-Control-Allow-Origin', process.env.CLIENT_SERVER_URL_HIGHLINK );
@@ -30,7 +41,7 @@ app.use((req, res, next) => {
     'X-Requested-With, Content-Type, Authorization'
   );
   next();
-});
+}); */
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
